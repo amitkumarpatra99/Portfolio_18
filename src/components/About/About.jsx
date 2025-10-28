da831a9a18dd2f147edd15578f8f68ed8f2d8444
AMIT KUMAR PATRA <amitkumarpatraaa@hotmail.com>
2025-10-28 20:19:48 +0530

    update Cv

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
           </p>
           {/* Resume */}
           <a
-            href="/src/assets/AMIT CV.pdf"
+            href="/src/assets/certificate/AMIT CV.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
