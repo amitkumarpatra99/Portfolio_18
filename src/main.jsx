2b94b41e8df419b0a9bde277d5bc70871cb74aae
AMIT KUMAR PATRA <amitkumarpatraaa@hotmail.com>
2025-12-07 16:07:43 +0530

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 
 createRoot(document.getElementById('root')).render(
-    <App />
-
-
+    <SpeedInsights>
+      <App />
+    </SpeedInsights>
 );
